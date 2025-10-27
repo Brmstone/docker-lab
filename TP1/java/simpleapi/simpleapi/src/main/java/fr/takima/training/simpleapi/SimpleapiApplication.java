6bd6f08421c778b77d5d051654bebcdad3406668
Brmstone <127938798+Brmstone@users.noreply.github.com>
2025-10-27 09:47:09 +0100

    ALEDDD

--- TP1/java/simpleapi/simpleapi/src/main/java/fr/takima/training/simpleapi/SimpleapiApplication.java
+++ TP1/java/simpleapi/simpleapi/src/main/java/fr/takima/training/simpleapi/SimpleapiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleApiApplication.class, args);
+		SpringApplication.run(SimpleapiApplication.class, args);
 	}
 
 }
